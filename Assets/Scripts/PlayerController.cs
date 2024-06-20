26712ef9b88a678cc1ba6076b1c65523e35fe2ac
DualDura <jdsan216@gmail.com>
2024-06-20 17:54:40 +0900

    첫 커밋 & 플레이어 캐릭터 생성

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
+    }
+
+}
