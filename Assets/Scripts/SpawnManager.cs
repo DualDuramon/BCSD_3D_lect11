2b5d96f971664881f2b7e107abca19f7ac4b20a6
DualDura <jdsan216@gmail.com>
2024-06-20 19:58:13 +0900

    백그라운드 스크롤링 및 장애물 파괴 로직 구현
    + 백그라운드 스크롤링 구현
    + 피한 장애물이 스스로 제거되는 로직 구현
    + 게임오버가 됐을 경우 장애물 생성 중단 로직 구현

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    public PlayerController playerControllerScript;
     private Vector3 spawnPos = new Vector3(25, 0, 0);   //스폰포인트 지정.
     private float startDelay = 2;
     private float repeatRate = 2;
@@ -14,10 +15,15 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);   //startDelay초 후에 repeatRate초 간격으로 SpawnObstacle함수 호출.
+        playerControllerScript 
+            = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);   //obstacle프리팹을 spawnPos에 생성.
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);   //obstacle프리팹을 spawnPos에 생성.
+        }
     }
 }
